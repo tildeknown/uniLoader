51b249d599fb0cfbee465347ea64924e887778f4
tildeguy <tildeguy@mainlining.org>
2026-01-16 16:54:45 +0300

    please no uart! !!! ! ! !!!! ! !!!! !!! ! ! ! !!!! ! !!!! ! ! ! !!!! ! ! !!!!! ! !!!!! !!! !! ! ! !!!! ! ! ! !!!! ! ! ! !!!! ! ! !!! ! ! ! wtf

--- board/huawei/board-agassi2.c
+++ board/huawei/board-agassi2.c
@@ -8,22 +8,6 @@
 #include <board.h>
 #include <drivers/framework.h>
 #include <lib/simplefb.h>
-#include <lib/debug.h>
-
-#define UART_BASE 0xfdf02000
-
-void uart_putc(char ch)
-{
-	// no uart for now
-}
-
-void uart_puts(const char *s)
-{
-	while (*s != '\0') {
-		uart_putc(*s);
-		s++;
-	}
-}
 
 static struct video_info agassi2_fb = {
 	.format = FB_FORMAT_ARGB8888,
